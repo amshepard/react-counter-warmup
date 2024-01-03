6c43352a939b3d181887627890c96d81c00622fa
Antoine M. Shepard <antoineshepard@pursuit.org>
2024-01-02 19:30:13 -0500

    adding functions to increment & multiply.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,22 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
+  // Function to increment count by 1
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  // Function to multiply count by 2
+  const multiplyCount = () => {
+    setCount(count * 2);
+  };
+
   return (
     <div className="App">
       <div className="number">{count}</div>
       <div className="buttons-container">
-        <div className="counter-button">+1</div>
-        <div className="counter-button blue">x2</div>
+        <div className="counter-button" onClick={incrementCount}>+1</div>
+        <div className="counter-button blue" onClick={multiplyCount}>x2</div>
       </div>
     </div>
   );
